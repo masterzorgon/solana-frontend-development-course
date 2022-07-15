e3a631f2ee539dac0567e9211a327199491eba4b
Nathan Galindo <nathan.zebedee@gmail.com>
2022-07-14 23:32:40 -0500

    fixed position of prev and nex buttons

--- components/StudentIntroList.tsx
+++ components/StudentIntroList.tsx
@@ -21,14 +21,6 @@ export const StudentIntroList: FC = () => {
     
     return (
         <div>
-            <div>
-                <input
-                    id='search'
-                    onChange={event => setSearch(event.currentTarget.value)}
-                    placeholder='Search'
-                    className='py-1 w-full text-white bg-transparent outline-none resize-none border-2 border-transparent border-b-white'
-                />
-            </div>
             <div className='mt-6'>
                 {
                     studentIntros.map((studentIntro, index) => (
@@ -48,21 +40,29 @@ export const StudentIntroList: FC = () => {
                 }
             </div>
             <div className='mt-6 flex justify-between'>
-                {page > 1 &&
-                    <button
-                        onClick={() => setPage(page - 1)}
-                        className='bg-[#fa6ece] rounded-lg px-4 py-1 font-semibold transition-all duration-200 hover:bg-transparent border-2 border-transparent hover:border-[#fa6ece]'
-                    >
-                        Previous
-                    </button>}
-
-                {StudentIntroCoordinator.accounts.length > page * 5 &&
-                    <button
-                        onClick={() => setPage(page + 1)}
-                        className='bg-[#fa6ece] rounded-lg px-4 py-1 font-semibold transition-all duration-200 hover:bg-transparent border-2 border-transparent hover:border-[#fa6ece]'
-                    >
-                        Next
-                    </button>}
+                <div className=''>
+                    {
+                        page > 1 &&
+                        <button
+                            onClick={() => setPage(page - 1)}
+                            className='bg-[#fa6ece] rounded-lg px-4 py-1 font-semibold transition-all duration-200 hover:bg-transparent border-2 border-transparent hover:border-[#fa6ece]'
+                        >
+                            Previous
+                        </button>
+                    }
+                </div>
+                
+                <div className=''>
+                    {
+                        StudentIntroCoordinator.accounts.length > page * 5 &&
+                        <button
+                            onClick={() => setPage(page + 1)}
+                            className='bg-[#fa6ece] rounded-lg px-4 py-1 font-semibold transition-all duration-200 hover:bg-transparent border-2 border-transparent hover:border-[#fa6ece]'
+                        >
+                            Next
+                        </button>
+                    }
+                </div>
             </div>
         </div>
     )
