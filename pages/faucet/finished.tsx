0565c96b1cbc5343cff348e17932192cd69aac3f
Nathan Galindo <nathan.zebedee@gmail.com>
2022-08-09 12:58:48 -0500

    added comments to teh Faucet project

--- pages/faucet/finished.tsx
+++ pages/faucet/finished.tsx
@@ -6,26 +6,34 @@ import * as web3 from '@solana/web3.js';
 
 const Finished = () => {
 
+    // allocate state to hold transaction signature
     const [txSig, setTxSig] = React.useState<string>('');
 
+    // get user info from wallet provider
     const { connection } = useConnection();
     const { publicKey, sendTransaction } = useWallet();
 
+    // function to send sol 
     const fundWallet = async (event: { preventDefault: () => void }) => {
+        // prevent page from refreshing when this function runs
         event.preventDefault();
 
+        // if user is not connected, throw an error
         if (!publicKey || !connection) {
             toast.error('Please connect your wallet');
             throw 'Please connect your wallet';
         }
 
+        // generate a new keypair 
         const sender = web3.Keypair.generate();
 
+        // check the balance of the keypair and send funds if needed
         const balance = await connection.getBalance(sender.publicKey);
         if (balance < web3.LAMPORTS_PER_SOL) {
             await connection.requestAirdrop(sender.publicKey, web3.LAMPORTS_PER_SOL * 1);
         }
 
+        // create a new transaction and add the instruction to transfer tokens
         const transaction = new web3.Transaction().add(
             web3.SystemProgram.transfer({
                 fromPubkey: sender.publicKey,
@@ -34,6 +42,7 @@ const Finished = () => {
             }),
         );
 
+        // send the transaction to the network
         try {
             const signature = await sendTransaction(transaction, connection, {
                 signers: [sender]
