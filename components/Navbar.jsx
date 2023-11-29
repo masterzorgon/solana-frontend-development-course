0a6cfacfa0e1ab629b252211f17ee15712f8322b
Nathan Galindo <nathan.galindo@tyson.com>
2023-11-29 22:32:42 +0100

    added hover state change to logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,16 +3,14 @@ import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 const Navbar = () => {
     return (
         <nav className='p-4 flex justify-between items-center bg-zinc-800'>
-            <a href="/" className='text-white hover:text-[#eb54bc] transition-all duration-200 flex items-center text-5xl'>
-                <img
-                    src='/helius.png'
-                    alt='logo'
-                    width='60'
-                />
+            <a href="/">
+                <div class="relative">
+                    <img src="helius-orange.png" width="60" className="transition-transform duration-300 transform hover:scale-100 hover:cursor-pointer" alt="Helius logo" />
+                    <img src="helius-white.png" width="60" className="absolute top-0 left-0 opacity-0 transition-opacity duration-300 transform hover:opacity-100 hover:cursor-pointer" alt="Helius logo" />
+                </div>
             </a>
-            <div>
-                <WalletMultiButton className='!bg-helius-orange hover:!bg-black transition-all duration-200 !rounded-lg' />
-            </div>
+
+            <WalletMultiButton className='!bg-helius-orange hover:!bg-black transition-all duration-200 !rounded-lg' />
         </nav>
     );
 };
