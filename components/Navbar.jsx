416c223537d0fb4e63ce6bb762a04b37798203c0
Nathan Galindo <nathan.galindo@tyson.com>
2023-11-29 22:35:08 +0100

    fix navbar missing logo bug

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
         <nav className='p-4 flex justify-between items-center bg-zinc-800'>
             <a href="/">
                 <div class="relative">
-                    <img src="helius-orange.png" width="60" className="transition-transform duration-300 transform hover:scale-100 hover:cursor-pointer" alt="Helius logo" />
-                    <img src="helius-white.png" width="60" className="absolute top-0 left-0 opacity-0 transition-opacity duration-300 transform hover:opacity-100 hover:cursor-pointer" alt="Helius logo" />
+                    <img src="/helius-orange.png" width="60" className="transition-transform duration-200 transform hover:scale-100 hover:cursor-pointer" alt="Helius logo" />
+                    <img src="/helius-white.png" width="60" className="absolute top-0 left-0 opacity-0 transition-opacity duration-200 transform hover:opacity-100 hover:cursor-pointer" alt="Helius logo" />
                 </div>
             </a>
 
