a367521728e6caebd82dc9bf9be6326c7a697985
Nathan Galindo <nathan.galindo@tyson.com>
2023-11-29 22:26:58 +0100

    added new logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,18 +3,13 @@ import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 const Navbar = () => {
     return (
         <nav className='p-4 flex justify-between items-center bg-zinc-800'>
-            <div>
-                <a href="/" className='text-white hover:text-[#eb54bc] transition-all duration-200 flex items-center text-5xl'>
-                    <img
-                        src='/official-logo.png'
-                        alt='logo'
-                        width='80'
-                    />
-                    <p className='ml-2 italic'>
-                        tbc
-                    </p>
-                </a>
-            </div>
+            <a href="/" className='text-white hover:text-[#eb54bc] transition-all duration-200 flex items-center text-5xl'>
+                <img
+                    src='/helius.png'
+                    alt='logo'
+                    width='60'
+                />
+            </a>
             <div>
                 <WalletMultiButton className='!bg-helius-orange hover:!bg-black transition-all duration-200 !rounded-lg' />
             </div>
