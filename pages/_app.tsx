35c56c0d6f3a843722a397650c701c2d14035c79
Nathan Galindo <nathangalindo@Nathans-MacBook-Pro.local>
2024-01-04 11:59:34 -0600

    edit metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,8 +13,8 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>TBC Labs</title>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐻</text></svg>" />
+        <title>Helius</title>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔨</text></svg>" />
       </Head>
       <WalletContextProvider>
         <Navbar />
